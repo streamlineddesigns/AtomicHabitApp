199ce273d86207408a2fdf4f04b56501af8e90ff
streamlineddesigns <streamline.our.designs@gmail.com>
2021-12-04 07:57:40 -0500

    Added shower result view

--- Assets/Scripts/UI/Controllers/Results/ColdShowersSpecificsController.cs
+++ Assets/Scripts/UI/Controllers/Results/ColdShowersSpecificsController.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace whm {
 
     public class ColdShowersSpecificsController : Controller
     {
-        
+        public Text DateTimeText;
+        public Text ColdShowerDurationText;
+
+        public void OnEnable()
+        {
+            DateTime date = new DateTime(UIController.instance.selectedResult.dateTime);
+            DateTimeText.text = date.ToString("MM/dd/yyyy HH:mm");
+
+            ColdShowerDurationText.text = UIController.instance.GetReadableTime(UIController.instance.selectedResult.intNumber);
+        }
     }
 
 }
